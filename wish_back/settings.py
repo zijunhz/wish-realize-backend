285f755d7ea2e3b79e963822e18b4bbba0126aff
truc0 <22969604+truc0@users.noreply.github.com>
2023-01-19 09:49:56 +0000

    feat(static): add static files to urls

--- wish_back/settings.py
+++ wish_back/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = getenv('WISH_STATIC_ROOT', 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
